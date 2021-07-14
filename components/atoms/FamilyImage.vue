118c3fc7b6fbdb7e6e2def7aea8a1bb3f75ec0c3
Mateus Ito Silva <mateusito10@gmail.com>
2021-07-14 03:45:04 -0300

    👥 | Family - add family section

---
+++ components/atoms/FamilyImage.vue
@@ -0,0 +1,87 @@
+<template>
+	<div class="family__overlay">
+		<img :src="img" alt="Family" :class="`family__image-${name}`" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['img', 'name']
+}
+</script>
+
+<style scoped>
+.family__overlay:nth-child(1) {
+	width: 263px;
+	height: 175px;
+	margin-right: 2rem;
+}
+
+.family__overlay:nth-child(2) {
+	width: 120px;
+	height: 180px;
+	position: absolute;
+	top: 2rem;
+	right: 0;
+}
+
+.family__overlay {
+	height: auto;
+	overflow: var(--img-hidden);
+}
+
+.family__image-one,
+.family__image-two {
+	object-fit: cover;
+	object-position: center;
+	height: 100%;
+	transition: var(--img-transition);
+}
+
+.family__image-one:hover,
+.family__image-two:hover {
+	transform: var(--img-scale);
+}
+
+/* Small Screens */
+@media screen and (max-width: 340px) {
+	.family__overlay:nth-child(1) {
+		width: 190px;
+		height: 126px;
+	}
+
+	.family__overlay:nth-child(2) {
+		width: 80px;
+		height: 120px;
+	}
+}
+
+/* Medium Screens */
+@media screen and (min-width: 768px) {
+	.family__overlay:nth-child(1) {
+		width: 363px;
+		height: 242px;
+		margin-right: 4rem;
+	}
+
+	.family__overlay:nth-child(2) {
+		width: 160px;
+		height: 240px;
+	}
+}
+
+/* Large Screens */
+@media screen and (min-width: 1024px) {
+	.family__overlay:nth-child(1) {
+		width: 463px;
+		height: 315px;
+		margin-right: 7rem;
+	}
+
+	.family__overlay:nth-child(2) {
+		width: 220px;
+		height: 330px;
+		top: 3rem;
+	}
+}
+</style>
---
+++ components/sections/Family.vue
@@ -0,0 +1,71 @@
+<template>
+	<section id="family" class="family section">
+		<div class="family__container container grid">
+			<!-- Family Data -->
+			<div class="family__data">
+				<h2 class="section__title family__title">
+					<rich-text-renderer
+						:document="blok.title"
+					></rich-text-renderer>
+				</h2>
+				<p class="family__description">
+					<rich-text-renderer
+						:document="blok.description"
+					></rich-text-renderer>
+				</p>
+			</div>
+
+			<!-- Family Images -->
+			<div class="family__image">
+				<FamilyImage :img="blok.imageOne.filename" name="one" />
+				<FamilyImage :img="blok.imageTwo.filename" name="two" />
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	props: {
+		blok: {
+			type: Object,
+			required: true
+		}
+	}
+}
+</script>
+
+<style scoped>
+.family__container {
+	row-gap: 2.5rem;
+	justify-content: center;
+	justify-items: center;
+}
+
+.family__image {
+	position: relative;
+	padding-bottom: 2rem;
+}
+
+/* Small Screens */
+@media screen and (max-width: 340px) {
+	.family__container {
+		padding: 0;
+	}
+
+	.family__img {
+		padding: 0;
+	}
+}
+
+/* Large Screens */
+@media screen and (min-width: 1024px) {
+	.family__container {
+		margin: var(--space-1) auto var(--space-1);
+	}
+
+	.family__description {
+		padding: 0 8rem;
+	}
+}
+</style>
