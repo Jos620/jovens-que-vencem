3689b91a39c56e1d218c99584c43ae161fd5b988
Mateus Ito <55610982+Jos620@users.noreply.github.com>
2021-07-19 21:04:55 -0300

    🦶🏻 | Footer - add footer component

---
+++ components/atoms/FooterList.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="footer__data">
+		<h3 class="footer__subtitle">{{ title }}</h3>
+
+		<ul class="footer__list">
+			<li class="footer__item" v-for="(item, i) in list" :key="i">
+				<a :href="item.link.url" class="footer__link">
+					{{ item.text }}
+				</a>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['title', 'list']
+}
+</script>
+
+<style scoped>
+ul {
+	margin-bottom: var(--space-2);
+}
+
+.footer__subtitle {
+	color: var(--title-color);
+	font-size: var(--h3-font-size);
+	margin-bottom: var(--space-1);
+}
+
+.footer__item {
+	margin-bottom: var(--space-0-75);
+}
+
+.footer__link {
+	color: var(--text-color);
+}
+
+.footer__link:hover {
+	color: var(--title-color);
+}
+</style>
---
+++ components/global/Footer.vue
@@ -0,0 +1,101 @@
+<template>
+	<footer id="footer" class="footer section">
+		<div class="footer__container container grid">
+			<div class="footer__content grid">
+				<div class="footer__data">
+                    <NLink to="/">
+                        <h3 class="footer__title">
+                            {{ blok.logo }}
+                        </h3>
+                    </NLink>
+					<p class="footer__description">
+						<rich-text-renderer :document="blok.description"></rich-text-renderer>
+					</p>
+
+					<div>
+						<SocialLink 
+                            class="footer__social" 
+                            v-for="socialLink in blok.social" 
+                            :key="socialLink._uid" 
+                            :link="socialLink.link.url" 
+                            :icon="socialLink.icon" 
+                        />
+					</div>
+				</div>
+
+				<FooterList
+					v-for="footerList in blok.footerLists"
+					:key="footerList._uid"
+					:title="footerList.title"
+					:list="footerList.list"
+				/>
+			</div>
+
+			<div class="footer__rights">
+				<p class="footer__copy">&#169; {{ year }} JQV - Campo Largo. Todos os direitos reservados.</p>
+			</div>
+		</div>
+	</footer>
+</template>
+
+<script>
+import { useNow } from '@vueuse/core'
+
+import useStoryblok from '~/composables/useStoryblok'
+
+export default {
+	setup() {
+        const { blok } = useStoryblok('components/footer')
+
+        const now = useNow()
+        const year = now.value.getFullYear()
+
+        return { blok, year }
+	}
+}
+</script>
+
+<style scoped>
+.footer__container {
+	row-gap: 5rem;
+}
+
+.footer__content {
+	grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+	row-gap: 2rem;
+}
+
+.footer__title {
+	font-size: var(--h2-font-size);
+	margin-bottom: var(--space-0-5);
+}
+
+.footer__description {
+	margin-bottom: var(--space-2);
+}
+
+.footer__social {
+	font-size: 1.25rem;
+	color: var(--title-color);
+	margin-right: var(--space-1-25);
+}
+
+.footer__rights {
+	display: flex;
+	flex-direction: column;
+	row-gap: 1.5rem;
+	text-align: center;
+}
+
+.footer__copy {
+	font-size: var(--small-font-size);
+	color: var(--text-color-light);
+}
+
+/* Large Screens */
+@media screen and (min-width: 1024px) {
+	.footer__content {
+		justify-items: center;
+	}
+}
+</style>
